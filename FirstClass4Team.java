a43ddb304ddd3920fe9194b9e4b904fda13ff904
kevin01157007 <152252977+kevin01157007@users.noreply.github.com>
2024-05-08 15:55:44 +0800

    feat:add code

---
+++ FirstClass4Team.java
@@ -0,0 +1,7 @@
+public class FirstClass4Team{
+    public static void main(String[] args)
+    {
+    System.out.println("這是用來練習GitHub協作的程式!");
+    System.out.println("\"學號：\"01157007\"\n姓名：\"邱顯成\"");
+    }
+}
